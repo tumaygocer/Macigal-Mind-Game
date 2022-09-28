9626690f0dbbc28635f74b9f8eb1edc3ae5cc0c2
Gocer615 <gtumayy21@gmail.com>
2022-09-28 17:18:59 +0300

    GameControl Scripts Button
    button click added and sprites

---
+++ Assets/GameControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void ButtonClicked(int value)
+    {
+        Debug.Log("Doðru" +  value);
+    }
+    void Update()
+    {
+        
+    }
+}
