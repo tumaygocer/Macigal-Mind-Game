bc053d0eef737411b45bdfa3731fba359e316bde
Gocer615 <gtumayy21@gmail.com>
2022-09-29 11:45:52 +0300

    Button Controller
    sprite replacement added

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -1,21 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
+
+
+    int firstchoicevalue;
+    GameObject ClickedObject;
+    public Sprite DefaultSprite;
+    GameObject SelectedButton;
     
+
+
+
     void Start()
     {
-        
+        firstchoicevalue = 0;
+    }
+
+    public void SelectObject(GameObject Objem)
+    {
+        ClickedObject = Objem;
+        ClickedObject.GetComponent<Image>().sprite = ClickedObject.GetComponentInChildren<SpriteRenderer>().sprite;
     }
 
     public void ButtonClicked(int value)
     {
-        Debug.Log("Doðru" +  value);
+        Control(value, ClickedObject);
     }
-    void Update()
+
+    void Control(int SelectedValue, GameObject SelectedObject)
     {
-        
+        if (firstchoicevalue == 0)
+        {
+            firstchoicevalue = SelectedValue;
+            SelectedButton = SelectedObject;
+        }
+        else
+        {
+            if (firstchoicevalue == SelectedValue)
+            {
+                Debug.Log("eþleþti");
+                firstchoicevalue = 0;
+            }
+            else
+            {
+                SelectedButton.GetComponent<Image>().sprite = DefaultSprite;
+                SelectedObject.GetComponent<Image>().sprite = DefaultSprite;
+                Debug.Log("eþleþmedi");
+                firstchoicevalue = 0;
+            }
+
+        }
     }
 }
