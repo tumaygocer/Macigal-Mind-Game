c5268048670aae2dab7df71253db63036879b93b
Gocer615 <gtumayy21@gmail.com>
2022-10-02 14:21:14 +0300

    refactor: Game Exit
    and game exit panel added

--- Assets/scripts/HomePageControl.cs
+++ Assets/scripts/HomePageControl.cs
@@ -5,7 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class HomePageControl : MonoBehaviour
 {
-   
+    public GameObject ExitPanel;
+    public void GameExit()
+    {       
+        ExitPanel.SetActive(true);
+    }
+
+    public void Reply(string reply)
+    {
+        
+        if (reply == "Yes")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            ExitPanel.SetActive(false);
+        }
+
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
